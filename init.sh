5e2d449c311029ff3635fce250f9972d744d7bbc
Le Nguyen Ky <ky.le@nfq.asia>
2020-04-04 12:43:16 +0700

    TODO-2: update readme & init project

---
+++ init.sh
@@ -0,0 +1,46 @@
+printf "\n ****************************** Step 1: Init docker ****************************** \n"
+
+ENV_PATH=$(pwd)/docker/.env
+
+printf "\nCopying .env.example to .env \n"
+cp $(pwd)/docker/.env.example $(pwd)/docker/.env
+
+export $(grep -v '^#' ${ENV_PATH} | xargs)
+
+printf "\n ****************************** Step 2: Docker build ****************************** \n"
+
+printf "\nMay take a long time (15min+) on the first run\n"
+
+docker-compose -f $(pwd)/docker/docker-compose.yml up --build -d
+
+printf "\n ****************************** Step 3: Init todolist ****************************** \n"
+
+ENV_PATH=$(pwd)/source/.env
+
+printf "\nCopying .env.example to .env \n"
+cp $(pwd)/source/.env.example $(pwd)/source/.env
+
+export $(grep -v '^#' ${ENV_PATH} | xargs)
+
+printf "\n ****************************** Step 4: Composer install ****************************** \n"
+
+docker exec -it -u laradock -w /var/www/ todolist_workspace_1 bash composer install
+
+printf "\n ****************************** Step 5: Composer migrate ****************************** \n"
+
+# docker exec -it -u laradock -w /var/www/ todolist_workspace_1 bash composer install
+
+cat << EOF
+
+######## #### ##    ## ####  ######  ##     ## ######## ########
+##        ##  ###   ##  ##  ##    ## ##     ## ##       ##     ##
+##        ##  ####  ##  ##  ##       ##     ## ##       ##     ##
+######    ##  ## ## ##  ##   ######  ######### ######   ##     ##
+##        ##  ##  ####  ##        ## ##     ## ##       ##     ##
+##        ##  ##   ###  ##  ##    ## ##     ## ##       ##     ##
+##       #### ##    ## ####  ######  ##     ## ######## ########
+
+
+EOF
+
+docker exec -it -u laradock -w /var/www/ todolist_workspace_1 bash
---
+++ readme.md
@@ -0,0 +1,18 @@
+### Prerequisite
+* [Docker](https://www.docker.com/products/docker-desktop)
+* [Git](https://git-scm.com/downloads)
+
+
+## Setup
+* Clone the project 
+  * Clone the project by running `git clone git@github.com:nguyenky/todolist.git`
+    
+* If you are using Unix (Mac, Linux), run `chmod +x ./init.sh`
+* Start `Docker`
+* Run `./init.sh`
+* Update your host config
+            
+            127.0.0.1	todo-list.local
+* Enter (http://todo-list.local/)
+* Done
+The first time it may take a little while to pull the images from Docker Hub and lots of confusing message printouts, but be patient. Give it some time.
